a8c89f540d8c5693a98e6d51727cabf72b5b613d
Kjawo <konjaworski@gmail.com>
2020-03-20 15:18:19 +0100

    Add missing params

--- CPS/Signal/Params.cs
+++ CPS/Signal/Params.cs
@@ -6,6 +6,9 @@
         public double T { get; set; } = 1;
         public double t1 { get; set; } = 0;
         public double d { get; set; } = 1;
+        public double kw { get; set; } = 0.5;
+        public double ts { get; set; } = 2;
+        public double p { get; set; } = 0.5;
 
     }
 }
